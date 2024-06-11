9368671d1679ee19c10801c12f207d94d1a81404
littlecm <clittle.norml@gmail.com>
2024-06-11 13:44:38 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
 
 module.exports = {
-  entry: './paymentCalculatorEntry.js',
+  entry: "./paymentCalculatorEntry.js",
   output: {
-    filename: 'paymentCalculator.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'PaymentCalculator',
-    libraryTarget: 'umd',
+    path: path.resolve(__dirname, "dist"),
+    filename: "paymentCalculatorBundle.js",
+    library: "PaymentCalculator",
+    libraryTarget: "umd",
+  },
+  resolve: {
+    fallback: {
+      assert: require.resolve("assert/"),
+      util: require.resolve("util/"),
+      buffer: require.resolve("buffer/"),
+    },
   },
   module: {
     rules: [
@@ -15,30 +21,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
+          loader: "babel-loader",
         },
       },
     ],
   },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    fallback: {
-      "zlib": require.resolve("browserify-zlib"),
-      "stream": require.resolve("stream-browserify"),
-      "crypto": require.resolve("crypto-browserify"),
-      "buffer": require.resolve("buffer"),
-      "assert": require.resolve("assert/"),
-      "util": require.resolve("util/"),
-      "fs": false // fs module is not meant for browser environment, so we can safely set it to false
-    }
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-      process: 'process/browser'
-    }),
-  ],
 };
