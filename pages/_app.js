d44836c70904c5dc2bea41ae3a34243dd2c9dea9
littlecm <clittle.norml@gmail.com>
2024-06-11 12:43:20 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import "../styles/globals.css";
+// pages/_app.js
+
+import React from 'react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
