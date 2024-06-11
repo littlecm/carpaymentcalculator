e360cc59d20a2b1376b1c441280b67a2f1ea283f
littlecm <clittle.norml@gmail.com>
2024-06-11 12:49:55 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -30,11 +30,15 @@ module.exports = {
       "stream": require.resolve("stream-browserify"),
       "crypto": require.resolve("crypto-browserify"),
       "buffer": require.resolve("buffer"),
+      "assert": require.resolve("assert/"),
+      "util": require.resolve("util/"),
+      "fs": false // fs module is not meant for browser environment, so we can safely set it to false
     }
   },
   plugins: [
     new webpack.ProvidePlugin({
       Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser'
     }),
   ],
 };
